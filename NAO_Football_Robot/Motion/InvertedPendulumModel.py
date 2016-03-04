b5d2a452875c8c9050df9dc9288026dae3a89a5b
andrei-sandu <andrei.sandu777@gmail.com>
2016-03-04 13:42:55 +0000

    Added constants for r parm and computation for (x0)y

--- NAO_Football_Robot/Motion/InvertedPendulumModel.py
+++ NAO_Football_Robot/Motion/InvertedPendulumModel.py
@@ -6,26 +6,23 @@ import motion
 
 
 class LIPM:
-    # TODO: assign a sensible value for this - "arbitrary param" - how?
-    ZMP_lateral_position = 0  # (x0)y
     # initialisation of other global parameters; needed here because good practice...
     ending_time = 0
     next_beginning_time = 0
     ZMP_forwards_velocity = 0
 
-    def __init__(self, proxy, beginning_time, curr_step_size, previous_step_size):
+    def __init__(self, proxy, beginning_time, curr_step_size, previous_step_size, support_foot):
         self.proxy = proxy
-        # Is it necessary to make this a field? YES! p(s) = (-(x(t)x, -(x(t))y, -h)!!!
-        self.pendulum_height = proxy.getCOM("Body", motion.FRAME_ROBOT, True)[2]
+        self.pendulum_height = proxy.getCOM("Body", motion.FRAME_ROBOT, True)[2] * RobotConstants.TO_MILLIMETERS
         self.k = math.sqrt(RobotConstants.GRAVITATIONAL_ACCELERATION / self.pendulum_height)
         self.beginning_time = beginning_time
-        #  TODO: Move r constants in RobotConstants?
-        self.r_lateral = RobotConstants.LEG_DISTANCE / 2.0  # ry
-        #  TODO: How do I initialise this?
-        self.r_walking_axis = 0  # rx
-        self.current_step_size = curr_step_size  # *NEXT* s; 3D vector; s[0] = *NEXT* sx, s[1] = *NEXT* sy s[2] = 0
-        # The next two are size 3 vectors
+        self.current_step_size = curr_step_size  # *NEXT* s; 2D vector; s[0] = *NEXT* sx, s[1] = *NEXT* sy s[2] = 0
         self.previous_step_size = previous_step_size
+        # (x0)y
+        initial_y_position = - proxy.getPosition(support_foot, motion.FRAME_TORSO, True)[1] + \
+            proxy.getCOM("Body", motion.FRAME_TORSO, True)[1] * RobotConstants.TO_MILLIMETERS
+        self.ZMP_lateral_position = initial_y_position / (math.cosh(self.k * beginning_time))
+        self.r = [RobotConstants.REF_PENDULUM_PIVOT_POINT_X, RobotConstants.LEG_DISTANCE/2]
 
     def get_next_support_phase_time(self):
         # take a guess of te
@@ -38,7 +35,7 @@ class LIPM:
             y = self.ZMP_lateral_position * math.cosh(self.k * self.ending_time) - \
                 (-self.ZMP_lateral_position) * math.cosh(self.k * self.next_beginning_time)
 
-            distance_between_pendulums = 2 * self.r_lateral + self.current_step_size[1]
+            distance_between_pendulums = 2 * self.r[1] + self.current_step_size[1]
 
             ending_time_velocity = self.ZMP_lateral_position * self.k * math.sinh(self.k * self.ending_time)
             offset = (distance_between_pendulums - y) / (2 * ending_time_velocity)
@@ -48,7 +45,7 @@ class LIPM:
         )
 
     def get_x_axis_component(self):
-        self.ZMP_forwards_velocity = (self.k * (2 * self.r_walking_axis + self.current_step_size[0])) / (
+        self.ZMP_forwards_velocity = (self.k * (2 * self.r[0] + self.current_step_size[0])) / (
             math.sinh(self.k * self.ending_time) - math.cosh(self.k * self.ending_time) *
             math.tanh(self.k * self.next_beginning_time)
         )
@@ -75,10 +72,10 @@ class LIPM:
                                               -self.pendulum_height], "non-supporting foot": [0, 0, 0]}
         q = self.get_non_supporting_foot_function(time)
         feet_positions["non-supporting foot"][0] = feet_positions["supporting foot"][0] + \
-            (2 * self.r_walking_axis + self.current_step_size[0]) * q - \
-            (self.previous_step_size[0] - 2 * self.r_walking_axis) * (1-q)
+            (2 * self.r[0] + self.current_step_size[0]) * q - \
+            (self.previous_step_size[0] - 2 * self.r[0]) * (1-q)
         feet_positions["non-supporting foot"][1] = feet_positions["supporting foot"][1] + \
-            (2 * self.r_lateral + self.current_step_size[1]) * q - \
-            (self.previous_step_size[1] - 2 * self.r_lateral) * (1-q)
+            (2 * self.r[1] + self.current_step_size[1]) * q - \
+            (self.previous_step_size[1] - 2 * self.r[1]) * (1-q)
         feet_positions["non-supporting foot"][2] = self.get_lifting_function(time)
         return feet_positions
