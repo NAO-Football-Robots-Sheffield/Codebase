856612cbe6401e70d4159c1567c7807345cd102c
andrei-sandu <andrei.sandu777@gmail.com>
2016-03-04 15:22:43 +0000

    Added CoM to Torso position conversion. Refactored method names in Pendulum Model, moved methods for readability, and added method to solve the inverted pendulum problem.

--- NAO_Football_Robot/Motion/InverseKinematics.py
+++ NAO_Football_Robot/Motion/InverseKinematics.py
@@ -1,12 +1,13 @@
 import almath
 import RobotConstants as robot
 import math
+import motion
 
 
 class InverseKinematics:
 
-    def __init__(self):
-        pass
+    def __init__(self, proxy):
+        self.proxy = proxy
 
     @staticmethod
     def generate_joint_angles(foot_position, foot):
@@ -52,3 +53,9 @@ class InverseKinematics:
         angles[foot.upper() + "HipPitch"] = math.atan2(-hip_orthogonal_to_thigh[8], hip_orthogonal_to_thigh[10])
         angles[foot.upper() + "HipYaw"] = math.atan2(-hip_orthogonal_to_thigh[1], hip_orthogonal_to_thigh[5])
         return angles
+
+    def com_to_torso_reference_change(self, position):
+        com_pos = self.proxy.getCOM("Body", motion.FRAME_TORSO, True)
+        torso_transform = almath.Transform.fromPosition(position[0], position[1], position[2]) * \
+            almath.Transform.fromPosition(com_pos[0], com_pos[1], com_pos[2])
+        return [torso_transform[3], torso_transform[7], torso_transform[11]]
--- NAO_Football_Robot/Motion/InvertedPendulumModel.py
+++ NAO_Football_Robot/Motion/InvertedPendulumModel.py
@@ -24,7 +24,7 @@ class LIPM:
         self.ZMP_lateral_position = initial_y_position / (math.cosh(self.k * beginning_time))
         self.r = [RobotConstants.REF_PENDULUM_PIVOT_POINT_X, RobotConstants.LEG_DISTANCE/2]
 
-    def get_next_support_phase_time(self):
+    def compute_next_support_phase_time(self):
         # take a guess of te
         self.ending_time = -self.beginning_time
         offset = 1
@@ -44,11 +44,29 @@ class LIPM:
             (self.ZMP_lateral_position * math.sinh(self.k * self.ending_time)) / (-self.ZMP_lateral_position)
         )
 
-    def get_x_axis_component(self):
+    def compute_zmp_forwards_velocity(self):
         self.ZMP_forwards_velocity = (self.k * (2 * self.r[0] + self.current_step_size[0])) / (
             math.sinh(self.k * self.ending_time) - math.cosh(self.k * self.ending_time) *
             math.tanh(self.k * self.next_beginning_time)
         )
+
+    def solve_pendulum_problem(self):
+        self.compute_next_support_phase_time()
+        self.compute_zmp_forwards_velocity()
+
+    def get_feet_positions(self, time):
+        feet_positions = {"supporting foot": [- self.get_position_x_component(time),
+                                              - self.get_position_y_component(time),
+                                              -self.pendulum_height], "non-supporting foot": [0, 0, 0]}
+        q = self.get_non_supporting_foot_function(time)
+        feet_positions["non-supporting foot"][0] = feet_positions["supporting foot"][0] + \
+            (2 * self.r[0] + self.current_step_size[0]) * q - \
+            (self.previous_step_size[0] - 2 * self.r[0]) * (1-q)
+        feet_positions["non-supporting foot"][1] = feet_positions["supporting foot"][1] + \
+            (2 * self.r[1] + self.current_step_size[1]) * q - \
+            (self.previous_step_size[1] - 2 * self.r[1]) * (1-q)
+        feet_positions["non-supporting foot"][2] = self.get_lifting_function(time)
+        return feet_positions
 
     def get_position_x_component(self, time):
         return self.ZMP_forwards_velocity * (1 / self.k) * math.sinh(self.k * time)
@@ -65,17 +83,3 @@ class LIPM:
     def get_lifting_function(self, time):
         lift_multiplier = 30  # Maximum foot height
         return math.sin(self.get_phase(time)) * lift_multiplier
-
-    def get_feet_positions(self, time):
-        feet_positions = {"supporting foot": [- self.get_position_x_component(time),
-                                              - self.get_position_y_component(time),
-                                              -self.pendulum_height], "non-supporting foot": [0, 0, 0]}
-        q = self.get_non_supporting_foot_function(time)
-        feet_positions["non-supporting foot"][0] = feet_positions["supporting foot"][0] + \
-            (2 * self.r[0] + self.current_step_size[0]) * q - \
-            (self.previous_step_size[0] - 2 * self.r[0]) * (1-q)
-        feet_positions["non-supporting foot"][1] = feet_positions["supporting foot"][1] + \
-            (2 * self.r[1] + self.current_step_size[1]) * q - \
-            (self.previous_step_size[1] - 2 * self.r[1]) * (1-q)
-        feet_positions["non-supporting foot"][2] = self.get_lifting_function(time)
-        return feet_positions
